1b33e183e47daaee5efb041788c49e0dc4476006
IsaacPar <88511157+IsaacPar@users.noreply.github.com>
2021-10-30 18:19:49 -0300

    this is typescript
    But No...

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-var me = document.getElementById("me")
+var me = document.getElementById("findcall")
+var findme = document.getElementById("winbox1")
 
 me.addEventListener('click', () => {
     mebox = new WinBox({
-        title: "About Me",
-        background: "#0000bd",
-        width: "400px",
+        title: "Find Me",
+        width: "500px",
         height: "400px",
-        top: 50,
+        top: 1,
         bottom: 1,
-        left: 50,
-        right: 50
+        left: 1,
+        right: 1,
+        mount: findme,
+        onfocus: function() {
+            this.setBackground("#0000bd")
+        },
+        onblur: function() {
+            this.setBackground("#888")
+        }
     })
 })
